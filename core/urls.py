17273a31bb04dfae861bcf77b08173496fb82d0d
Vivaan Sakaria <vivaansakaria007@gmail.com>
2026-06-12 11:08:45 +0530

    My local changes

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('doctor-login/', views.doctor_login),
-    path('doctor-register/', views.doctor_register),
-    path('patient-login/', views.patient_login),
-    path('patient-register/', views.patient_register),
+
+    # Home
+    path('', views.home, name='home'),
+
+    # Doctor
+    path('doctor-login/', views.doctor_login, name='doctor_login'),
+    path('doctor-register/', views.doctor_register, name='doctor_register'),
+    path('doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
+
+    # Patient
+    path('patient-login/', views.patient_login, name='patient_login'),
+    path('patient-register/', views.patient_register, name='patient_register'),
+    path('patient-dashboard/', views.patient_dashboard, name='patient_dashboard'),
+
 ]
